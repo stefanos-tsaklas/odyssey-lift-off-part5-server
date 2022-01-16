71e12db3564e7e075a543698d6af16a27ec694bb
Stefanos Tsaklas <stsaklas@microsoft.com>
2022-01-16 22:29:04 +0100

    Swapped apollo-server for apollo-server-express

--- src/index.js
+++ src/index.js
@@ -1,25 +1,45 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const express = require('express');
+const http = require('http');
+
+const TrackAPI = require('./datasources/track-api');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-const TrackAPI = require('./datasources/track-api');
 
 async function startApolloServer(typeDefs, resolvers) {
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    dataSources: () => {
-      return {
-        trackAPI: new TrackAPI(),
-      };
-    },
-  });
 
-  const { url, port } = await server.listen({port: process.env.PORT || 4000});
-  console.log(`
-      🚀  Server is running
-      🔉  Listening on port ${port}
-      📭  Query at ${url}
-    `);
+    // Required logic for integrating with Express
+    const app = express();
+    const httpServer = http.createServer(app);
+
+    // Same ApolloServer initialization as before, plus the drain plugin.
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      dataSources: () => {
+        return {
+          trackAPI: new TrackAPI(),
+        };
+      },
+      plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+
+      // More required logic for integrating with Express
+    await server.start();
+
+    // server.applyMiddleware({ app, ...rest }) is shorthand for app.use(server.getMiddleware(rest))
+    server.applyMiddleware({
+      app,
+      // By default, apollo-server hosts its GraphQL endpoint at the
+      // server root. However, *other* Apollo Server packages host it at
+      // /graphql. Optionally provide this to match apollo-server.
+      path: '/'
+    });
+  
+    const port = process.env.PORT || 4000;
+    await new Promise(resolve => httpServer.listen({port}, resolve));
+    console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
 }
 
 startApolloServer(typeDefs, resolvers);
--- src/schema.js
+++ src/schema.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server');
+const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type Query {
@@ -13,6 +13,11 @@ const typeDefs = gql`
   type Mutation {
     "Increment the number of views of a given track, when the track card is clicked"
     incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+  }
+
+  type Subscription {
+    "Happens when user views one page"
+    userViewed: String
   }
 
   type IncrementTrackViewsResponse {
